b449e9c0e1cf4dab71374364560f0246fc8f67aa
Gaurav Subedi <subedi.gasu15@gmail.com>
2019-06-01 13:59:10 +0545

    Committed from Pycharm

--- classifier.py
+++ classifier.py
@@ -10,21 +10,21 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Convolution2D(filters=32, kernel_size=(3,3), input_shape = (64,64,3), activation='relu'))
+classifier.add(Convolution2D(filters=32, kernel_size=(3, 3), input_shape=(64, 64, 3), activation='relu'))
 
 # Step 2 - Pooling
-classifier.add(MaxPooling2D(pool_size=(2,2), strides=2))
+classifier.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 
 # Adding a second convolutional layer for performance improvement
-classifier.add(Convolution2D(filters=32, kernel_size=(3,3), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2,2), strides=2))
+classifier.add(Convolution2D(filters=32, kernel_size=(3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full Connection
-classifier.add(Dense(units= 128, activation= 'relu'))
-classifier.add(Dense(units= 1, activation= 'sigmoid'))
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
 
 # Compiling the CNN
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
@@ -33,17 +33,17 @@ classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accur
 from keras.preprocessing.image import ImageDataGenerator
 
 train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                target_size=(64, 64),
-                                                batch_size=32,
-                                                class_mode='binary')
+                                                 target_size=(64, 64),
+                                                 batch_size=32,
+                                                 class_mode='binary')
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size=(64, 64),
@@ -52,7 +52,7 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
 
 # TRAINING THE CLASSIFIER
 classifier.fit_generator(training_set,
-                        steps_per_epoch=8000,
-                        epochs=25,
-                        validation_data=test_set,
-                        validation_steps=2000)
+                         steps_per_epoch=8000,
+                         epochs=25,
+                         validation_data=test_set,
+                         validation_steps=2000)
